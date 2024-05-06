68bb928259b8dc8da1289689a618b69b89d3968b
viniesqui <viniesqui@gmail.com>
2024-05-06 16:36:11 -0600

    error en el commit pasado

--- api.py
+++ api.py
@@ -16,7 +16,7 @@ model = joblib.load('trained_model.pkl')
 # Definir la ruta de la API y los CRUDS
 @app.post('/predict')
 def predict(data: InputData):
-    input_data = np.array([data.feature1, data.feature2])  # mterle los features
+    input_data = np.array([data.TV, data.Sales])  # mterle los features
 
     # predecir
     prediction = model.predict([input_data])
